968d19f34b1a93e4993ace1584c508f5346ff3b1
Melsteinn <lukadomanchuk@gmail.com>
2026-04-26 23:23:51 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,12 @@
 #if !defined(GEODE_IS_IOS)
 #include <Geode/Geode.hpp>
-#include <geode.custom-keybinds/include/Keybinds.hpp>
 #include <sabe.persistenceapi/include/PersistenceAPI.hpp>
 
 using namespace geode::prelude;
 using namespace persistenceAPI;
 
-void setupKeybinds();
-
 $on_mod(Loaded) {
-    setupKeybinds();
-}
-
-void setupKeybinds() {
-    using namespace keybinds;
-
-    BindManager::get()->registerBindable({
-        "save-game"_spr,
-        "Save game",
-        "Hotkey for quick saving",
-        { Keybind::create(KEY_K, Modifier::None) },
-        Category::PLAY,
-    });
+    // Keybinds are now handled natively by Geode 5.x
+    // The save-game keybind is registered in PlayLayer::setupKeybinds()
 }
 #endif
